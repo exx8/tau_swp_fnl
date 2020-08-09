2a48b2d28c30baeb80509c9072b160d62d84c017
exx8 <exx8eran@gmail.com>
2020-08-09 18:33:37 +0300

    splits and edge Arr build

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <assert.h>
+#include <stdlib.h>
+#define intsize 4
+struct _networkStats{
+    int vertices;
+    int edges;
+} typedef networkStats;
+struct _edge{
+    int highIndex;
+    int lowIndex;
+} typedef edge;
+
+void* memory(int num,size_t size){
+    void* buffer=calloc(num,size);
+    assert(buffer!=NULL);
+    return buffer;
+
+}
+int getVertices(const FILE *file);
+networkStats getNetworkStats (FILE* file, int fileLengthInBytes);
 
 int filesize(char *filePath) {
     struct stat details;
@@ -12,20 +31,61 @@ int filesize(char *filePath) {
     assert(status!=0);
     return -1;
 }
+int readInt(const FILE *file, const int *intPointer) {
+    fscanf (file, "%d", intPointer);
+}
  void readInputFile(char *filePath)
 {
-    const fileLength=filesize(filePath);
+    const fileLengthInBytes=filesize(filePath);
     const FILE* file=fopen(filePath,"r");
+    networkStats networkStat=getNetworkStats(file,fileLengthInBytes);
 
+    edge* edgeArr=memory(networkStat.edges,sizeof(edge));
+    edge* edgePointer=edgeArr;
+    while(feof(file)==0)
+    {
+        int edgePrimaryIndex=0,verticesLeft;
+        readInt(file,&verticesLeft);
+        while(verticesLeft > 0)
+        {
+            int edgeNeighborIndex=0;
+            readInt(file, edgeNeighborIndex);
+            if(edgeNeighborIndex > edgePrimaryIndex) {
+                (edgePointer)->highIndex = edgeNeighborIndex;
+                (edgePointer)->lowIndex = edgePrimaryIndex;
+            }
+            edgePointer++;
+            verticesLeft--;
+        }
+        edgePrimaryIndex++;
+
+    }
     fclose(file);
 }
 
-int getNetworkStats(FILE* file){
+
+
+
+ networkStats getNetworkStats (FILE* file, int fileLengthInBytes){
+     networkStats networkStat;
+    int verticesNum = getVertices(file);
+    const int edgesNum = (fileLengthInBytes - verticesNum - 1) / 2;
+    networkStat.vertices=verticesNum;
+    networkStat.edges=edgesNum;
+    return networkStat;
+
 
 }
 
+
+
+int getVertices(const FILE *file) {
+    const int verticesNum = 0;
+    readInt(file, &verticesNum);
+    return verticesNum;
+}
+
 int main() {
-    int s = filesize("/home/eran/CLionProjects/tau_swp_pca/m2ain.c");
     printf("Hello, World!\n");
     return 0;
 }
