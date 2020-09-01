734bc76c12322677f962d8599e46113ca249b61e
exx8 <exx8eran@gmail.com>
2020-09-01 23:36:58 +0300

    calcualte bilinear multipilication between A and vector.

---
+++ algo2.c
@@ -0,0 +1,53 @@
+//
+// Created by eran on 01/09/2020.
+//
+typedef int bool;
+#define true 1
+#define false 0
+
+#include "ds.h"
+#include "networkStats.h"
+
+int bilinearMultipicationOfB(rowLinkedList *Ag, networkStatsSet *AgStat, networkStatsSet *AGlobalstats,
+                              int *eigenVectorApproximation) {
+    //@todo check me!!!
+    int bilinearValue=0;
+    const M = AGlobalstats->degreeSum;
+    const vectorLength = AgStat->vertices;
+    rowLinkedList *AgCurrent = Ag;
+    double *partialMultipicationOfEigenAndMatrix = memory(sizeof(double), vectorLength);
+    int rowIndex, colIndex;
+// all matrices are symmetrical.
+    for (rowIndex = 0; rowIndex < vectorLength; rowIndex++) {
+        bool isRowExists = AgCurrent->rowIndex == rowIndex ? 1 : 0;
+
+        int sum = 0;
+        colLinkedList *AgCurrentCol = AgCurrent->colList;
+
+        for (colIndex = 0; colIndex < vectorLength; colIndex++) {
+            int B_ij = 0;
+            const bool isColExists = AgCurrentCol->colIndex == colIndex ? 1 : 0;
+            const bool isCellExists = isRowExists && isColExists;
+
+            B_ij -= AGlobalstats->vertexDegreeArray[rowIndex] * AGlobalstats->vertexDegreeArray[colIndex] / M;
+            if (isCellExists) {
+                B_ij++;//Add 1 exists
+                AgCurrentCol = AgCurrentCol->next;
+            }
+            partialMultipicationOfEigenAndMatrix[rowIndex] = eigenVectorApproximation[rowIndex * B_ij];
+        }
+
+        partialMultipicationOfEigenAndMatrix[rowIndex] = sum;
+        if (rowIndex == AgCurrent->rowIndex) {
+            AgCurrent = AgCurrent->nextRow;
+        }
+    }
+    for(rowIndex=0;rowIndex<vectorLength;rowIndex++)
+    {
+        bilinearValue+=eigenVectorApproximation[rowIndex]*partialMultipicationOfEigenAndMatrix[rowIndex];
+    }
+
+    free(partialMultipicationOfEigenAndMatrix);
+    return bilinearValue;
+
+}
