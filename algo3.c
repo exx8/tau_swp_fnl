5886d13f3fad036e063df524929bfe9c2907609e
exx8 <exx8eran@gmail.com>
2020-09-16 00:26:53 +0300

    linting

--- algo3.c
+++ algo3.c
@@ -8,42 +8,38 @@
 #include "algo2.h"
 
 
-
-
-
-void addNodeToBeginning(communityDescription* communityInfo, communitiesList* list){
-    communitiesList* newGroup;
+void addNodeToBeginning(communityDescription *communityInfo, communitiesList *list) {
+    communitiesList *newGroup;
     newGroup->communityInfo = communityInfo;
     newGroup->next = &list;
     list = newGroup;
 }
 
-communitiesList* algo3(communityDescription community) {
+communitiesList *algo3(communityDescription community) {
 
-    communitiesList* groupA;
-    communitiesList* groupB;
+    communitiesList *groupA;
+    communitiesList *groupB;
     groupA->communityInfo = &community;
-    while(groupA != NULL){
-        communitiesList* groupC = &groupA;
-        communityDescription * algo2Division [2]= algo2(groupC->communityInfo);
+    while (groupA != NULL) {
+        communitiesList *groupC = &groupA;
+        communityDescription *algo2Division[2] = algo2(groupC->communityInfo);
         groupA = groupA->next;
         int firstGroupVetricesNum = algo2Division[0]->networkStat.vertices;
         int secondGroupVerticesNum = algo2Division[1]->networkStat.vertices;
-        if(firstGroupVetricesNum == 0 || secondGroupVerticesNum == 0){
+        if (firstGroupVetricesNum == 0 || secondGroupVerticesNum == 0) {
             groupC->next = groupB;
             groupB = groupC;
-        }
-        else{
-            if(firstGroupVetricesNum == 1 ){
+        } else {
+            if (firstGroupVetricesNum == 1) {
                 addNodeToBeginning(algo2Division[0], groupB);
             }
-            if(secondGroupVerticesNum == 1 ){
+            if (secondGroupVerticesNum == 1) {
                 addNodeToBeginning(algo2Division[1], groupB);
             }
-            if(algo2Division[0]->networkStat.vertices > 1 ){
+            if (algo2Division[0]->networkStat.vertices > 1) {
                 addNodeToBeginning(algo2Division[0], groupA);
             }
-            if(secondGroupVerticesNum > 1 ){
+            if (secondGroupVerticesNum > 1) {
                 addNodeToBeginning(algo2Division[1], groupA);
             }
         }
