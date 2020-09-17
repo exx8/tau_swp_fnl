8983b01741be6d4c0243c4813f5701e029e0663a
Master Of None <nezar.ras@gmail.com>
2020-09-17 19:09:47 +0300

    Merge branch 'matrix_shifting' of https://github.com/exx8/tau_swp_fnl into matrix_shifting
    # Conflicts:
    #	main.c

--- shift.c
+++ shift.c
@@ -12,11 +12,11 @@ therefore we find maximum of rows */
 
 double findMaxColSum(rowLinkedListSparseMatrix * sparseMatrix){
     double result = 0;
-    while (sparseMatrix!= NULL){
+    while (sparseMatrix != NULL){
         double colSum = 0;
         colLinkedListSparseMatrix * currNode = sparseMatrix->colList;
         while (currNode != NULL){
-            colSum += currNode->value;
+            colSum += abs(currNode->value);
             currNode = currNode->next;
         }
         if (result < colSum){
